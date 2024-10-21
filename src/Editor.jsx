64684579baa020e509d2cdd5510dde7ea34af89b
Andrew Barraford <andy.barraford@gmail.com>
2024-10-21 08:42:15 +0200

    Toolbar state

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -1,20 +1,14 @@
 /** @jsxImportSource @emotion/react */
-import { Global, css } from '@emotion/react';
-import {
-    forwardRef,
-    useCallback,
-    useContext,
-    useEffect,
-    useLayoutEffect,
-    useRef,
-    useState
+import {Global, css} from '@emotion/react';
+import React, {
+    forwardRef, useCallback, useContext, useEffect, useLayoutEffect, useRef
 } from 'react';
 import Quill from 'quill';
 import 'quill/dist/quill.snow.css'; // Ensure Quill CSS is imported for proper styling
 import styled from '@emotion/styled';
-import { AppContext } from './AppContext.jsx';
+import {AppContext} from './AppContext.jsx';
 import DesktopToolbarActions from './toolbar/DesktopToolbarActions';
-import { useTheme } from '@mui/material';
+import {alpha, useTheme} from '@mui/material';
 import PropTypes from 'prop-types';
 
 // Styled components for the editor container and wrapper
@@ -28,6 +22,7 @@ const QuillEditorWrapper = styled.div`
     height: 100%;
     width: 100%;
     position: relative; /* Ensure that the pseudo-element is positioned correctly */
+
     .ql-editor {
         min-height: 100%;
         padding-left: 2.5em;
@@ -35,25 +30,36 @@ const QuillEditorWrapper = styled.div`
 `;
 
 const Editor = forwardRef(({
-                               readOnly,
-                               defaultValue,
-                               onTextChange,
-                               onSelectionChange
+                               readOnly, defaultValue, onTextChange, onSelectionChange
                            }, ref) => {
     const containerRef = useRef(null);
     const defaultValueRef = useRef(defaultValue);
     const onTextChangeRef = useRef(onTextChange);
     const onSelectionChangeRef = useRef(onSelectionChange);
-    const { state } = useContext(AppContext);
-    const theme = useTheme();
 
-    const [editorReady, setEditorReady] = useState(false);
+    const [currentHeader, setCurrentHeader] = React.useState(0);
+    const [currentBold, setCurrentBold] = React.useState(false);
+    console.log(currentBold)
+    const {state} = useContext(AppContext);
+    const theme = useTheme();
 
     const handleTextChange = useCallback(() => {
         if (ref.current) {
             onTextChangeRef.current?.(ref.current.getText());
         }
     }, [ref]);
+
+    const undo = useCallback(() => {
+        if (ref.current) {
+            ref.current.history.undo();
+        }
+    }, [ref])
+
+    const redo = useCallback(() => {
+        if (ref.current) {
+            ref.current.history.redo();
+        }
+    }, [ref])
 
     useLayoutEffect(() => {
         onTextChangeRef.current = onTextChange;
@@ -76,23 +82,23 @@ const Editor = forwardRef(({
         const editorContainer = container.appendChild(container.ownerDocument.createElement('div'));
 
         const quill = new Quill(editorContainer, {
-            theme: state.appSettings.quillTheme || 'snow', // Ensure a valid theme is applied
-            modules: {
-                toolbar: false,
-                history: {
-                    delay: 1000,
-                    maxStack: 100,
-                    userOnly: false,
+            formats: ['header', 'bold', 'italic', 'underline', 'background', 'blockquote'], modules: {
+                toolbar: {
+                    container: "#toolbar", handlers: {
+                        "undo": undo, "redo": redo,
+                    }
+                }, history: {
+                    delay: 1000, maxStack: 100, userOnly: false,
                 },
             },
-            formats: ['header', 'bold', 'italic', 'underline', 'background', 'blockquote'],
+
         });
 
         ref.current = quill;
 
         if (defaultValueRef.current) {
             quill.setContents(defaultValueRef.current);
-            quill.history.clear(); // Clear history to remove default state changes
+            quill.history.clear();
         }
 
         // Listen for text changes and update line numbers
@@ -103,13 +109,23 @@ const Editor = forwardRef(({
             highlightActiveLine(quill, range);
         });
 
-        setEditorReady(true);
+        quill.on('editor-change', () => {
+
+            const range = quill.getSelection();
+            if (range) {
+                const format = quill.getFormat(range);
+                setCurrentBold(format.bold);
+                setCurrentHeader(format.header);
+            }
+        });
+
 
         return () => {
             ref.current = null;
-            container.innerHTML = ''; // Clean up the container content on unmount
+            container.innerHTML = '';
         };
-    }, [handleTextChange, ref, state.appSettings.quillTheme]);
+    }, [handleTextChange, redo, ref, state.appSettings.quillTheme, undo]);
+
 
     // Function to highlight the active line
     const highlightActiveLine = (quillInstance, range) => {
@@ -128,13 +144,70 @@ const Editor = forwardRef(({
         }
     };
 
-    // Dynamically generate CSS for line numbers and active line highlighting
-    const dynamicStyles = css`
+    // Custom toolbar functions
+    const handleHeadingChange = (event) => {
+        const value = event.target.value;
+        const quill = ref.current;
+        if (quill) {
+            quill.format('header', value);
+        }
+    };
+
+    return (<div>
+        {/* Inject dynamic CSS for line numbers and active line highlighting */}
+        <Global styles={dynamicStyles(theme)}/>
+
+        <div id={"toolbar"} className={"ql-toolbar"}>
+            <DesktopToolbarActions
+                currentHeader={currentHeader}
+                currentBold={currentBold}
+                quillRef={ref}
+                handleHeader={handleHeadingChange}
+                isAdvanced={true}
+                handleSave={() => {
+                }}
+                savingBookContent={false}
+            />
+        </div>
+
+        <EditorContainer>
+            <QuillEditorWrapper className="quill-editor-wrapper">
+                <div ref={containerRef}></div>
+            </QuillEditorWrapper>
+        </EditorContainer>
+    </div>);
+});
+
+Editor.displayName = 'Editor';
+
+Editor.propTypes = {
+    readOnly: PropTypes.bool.isRequired,
+    defaultValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.object]),
+    onTextChange: PropTypes.func.isRequired,
+    onSelectionChange: PropTypes.func.isRequired,
+};
+
+export default Editor;
+
+const dynamicStyles = (theme) => {
+    return css`
         /* Line Numbers */
+
         .ql-editor {
             counter-reset: line;
             position: initial;
         }
+
+        .ql-active {
+            border: solid 1px ${alpha(theme.palette.primary.main, 0.5)} !important;
+            background-color: ${alpha(theme.palette.primary.main, 0.5)} !important;
+            border-radius: 3px;
+        }
+
+        .ql-active:hover {
+            background-color: ${theme.palette.primary.main} !important;
+        }
+
         .ql-editor p::before,
         .ql-editor h1::before,
         .ql-editor h2::before,
@@ -157,7 +230,9 @@ const Editor = forwardRef(({
             border-right: none; /* Remove border-right to avoid overlapping with vertical border */
             z-index: 2; /* Ensure line numbers appear above the gutter border */
         }
+
         /* Adjust paragraph and heading position to make space for line numbers */
+
         .ql-editor p,
         .ql-editor h1,
         .ql-editor h2,
@@ -169,7 +244,7 @@ const Editor = forwardRef(({
             position: initial;
             margin-left: 0;
         }
-        /* Handle empty paragraphs and headings */
+
         .ql-editor p:empty::before,
         .ql-editor h1:empty::before,
         .ql-editor h2:empty::before,
@@ -181,12 +256,12 @@ const Editor = forwardRef(({
             content: counter(line);
         }
 
-        /* Active Line Highlighting */
         .active-line {
             background-color: ${theme.palette.action.hover || 'rgba(0, 0, 0, 0.04)'};
         }
 
         /* Continuous Vertical Border for Gutter */
+
         .quill-editor-wrapper::before {
             content: '';
             position: absolute;
@@ -198,70 +273,4 @@ const Editor = forwardRef(({
             z-index: 1;
         }
     `;
-
-    // Custom toolbar functions
-    const handleHeadingChange = (event) => {
-        const value = event.target.value;
-        const quill = ref.current;
-        if (quill) {
-            quill.format('header', value);
-        }
-    };
-
-    const handleUndo = () => ref.current?.history.undo();
-    const handleRedo = () => ref.current?.history.redo();
-    const handleBold = () => ref.current?.format('bold', true);
-    const handleItalic = () => ref.current?.format('italic', true);
-    const handleUnderline = () => ref.current?.format('underline', true);
-    const handleHighlight = () => ref.current?.format('background', 'yellow');
-    const handleQuote = () => ref.current?.format('blockquote', true);
-    const handleClearFormatting = () => {
-        const selection = ref.current?.getSelection();
-        if (selection) {
-            ref.current?.removeFormat(selection.index, selection.length);
-        }
-    };
-
-    return (
-        <div>
-            {/* Inject dynamic CSS for line numbers and active line highlighting */}
-            <Global styles={dynamicStyles} />
-
-            {editorReady && (
-                <DesktopToolbarActions
-                    quillRef={ref}
-                    handleUndo={handleUndo}
-                    handleRedo={handleRedo}
-                    handleBold={handleBold}
-                    handleItalic={handleItalic}
-                    handleHeader={handleHeadingChange}
-                    handleUnderline={handleUnderline}
-                    handleHighlight={handleHighlight}
-                    handleQuote={handleQuote}
-                    handleClearFormatting={handleClearFormatting}
-                    isAdvanced={true}
-                    isReadOnly={readOnly}
-                    handleSave={() => { }}
-                    savingBookContent={false}
-                />
-            )}
-
-            <EditorContainer>
-                <QuillEditorWrapper className="quill-editor-wrapper">
-                    <div ref={containerRef}></div>
-                </QuillEditorWrapper>
-            </EditorContainer>
-        </div>
-    );
-});
-
-Editor.displayName = 'Editor';
-
-Editor.propTypes = {
-    readOnly: PropTypes.bool.isRequired,
-    defaultValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.object]),
-    onTextChange: PropTypes.func.isRequired,
-    onSelectionChange: PropTypes.func.isRequired,
-};
-
-export default Editor;
+}
