5fbace1870ff6dc1fa7b91a1b53de9c2b9999261
abarrafo <abarrafo@users.noreply.github.com>
2024-11-03 10:26:49 +0100

    Ab strip down (#7)
    * Pull out themeing in favor of parent theme provider. Update packaging, optimizing bundle.

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// index.js (this will be your entry point for library exports)
+import App from './App';
+import Editor from './Editor';
+
+export { App, Editor };
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: './src/index.js',  // Point to the file where all components are exported
+      name: 'BookcicleEditor',
+      fileName: (format) => `bookcicle-editor.${format}.js`,
+      formats: ['es', 'cjs'], // Export as ES module and CommonJS
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'quill'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          quill: 'Quill',
+        },
+      },
+    },
+  },
+});
