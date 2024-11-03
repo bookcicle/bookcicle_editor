435252e4601ecfa4bb072259ef147fc12be7bdd1
Andrew Barraford <andy.barraford@gmail.com>
2024-11-03 11:09:25 +0100

    Updates to packaging

--- src/index.js
+++ src/index.js
@@ -1,5 +1,2 @@
-// index.js (this will be your entry point for library exports)
-import App from './App';
-import Editor from './Editor';
-
-export { App, Editor };
+export { default as Editor } from './Editor';
+export { default as App } from './App';
