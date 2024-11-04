05b3e6700e3da132ca12fb146b893e92be032d43
Andrew Barraford <andy.barraford@gmail.com>
2024-11-04 11:10:39 +0100

    Customized Toolbar buttons, formula support, and adding editor-settings toggles to enable/disable features.

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@
 import {useRef} from 'react';
 import Editor from './Editor';
 import Quill from 'quill';
-import './App.css';
-import {Container, CssBaseline} from "@mui/material";
+import {Container, createTheme, CssBaseline, ThemeProvider} from "@mui/material";
+
 const Delta = Quill.import('delta');
 
 const App = () => {
@@ -12,25 +12,27 @@ const App = () => {
     const quillRef = useRef(null);
 
     return (<main>
-        <CssBaseline/>
-        <Container maxWidth={false} style={{padding: 0}}>
-            <Editor
-                ref={quillRef}
-                readOnly={false}
-                defaultValue={new Delta()
-                    .insert('Hello')
-                    .insert('\n', {header: 1})
-                    .insert('Some ')
-                    .insert('initial', {bold: true})
-                    .insert(' ')
-                    .insert('content', {underline: true})
-                    .insert('\n')}
-                onSelectionChange={() => {
-                }}
-                onTextChange={() => {
-                }}
-            />
-        </Container>
+        <ThemeProvider theme={createTheme({palette: {mode: "dark"}})}>
+            <CssBaseline/>
+            <Container maxWidth={false} style={{padding: 0}}>
+                <Editor
+                    ref={quillRef}
+                    readOnly={false}
+                    defaultValue={new Delta()
+                        .insert('Hello')
+                        .insert('\n', {header: 1})
+                        .insert('Some ')
+                        .insert('initial', {bold: true})
+                        .insert(' ')
+                        .insert('content', {underline: true})
+                        .insert('\n')}
+                    onSelectionChange={() => {
+                    }}
+                    onTextChange={() => {
+                    }}
+                />
+            </Container>
+        </ThemeProvider>
     </main>);
 };
 
